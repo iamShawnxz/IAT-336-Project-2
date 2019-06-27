42aa6eb71478204fba6b3e421aa334fd7344d502
Shawn <shawnxiaozhang95@gmail.com>
2019-06-27 02:28:44 -0700

    JavaScript loading

---
+++ Website/js/p2.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
